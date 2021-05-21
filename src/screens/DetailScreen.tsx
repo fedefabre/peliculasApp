a48714cf0138d2aae9e9a00869079183464225d5
Federico-Fabre <federico.fabre@xpo.com>
2021-05-21 07:31:06 -0300

    Adding Icons

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,19 +1,79 @@
 import { StackScreenProps } from '@react-navigation/stack'
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image, StyleSheet, Dimensions, ScrollView } from 'react-native';
 import { RouteStackParams } from '../navigation/Navigation';
 
-interface Props extends StackScreenProps<RouteStackParams, 'DetailScreen'> {}
+import Icon from 'react-native-vector-icons/Ionicons';
 
-const DetailScreen = ({ route }: Props) => {
+const screenHeight = Dimensions.get('screen').height;
+
+interface Props extends StackScreenProps<RouteStackParams, 'DetailScreen'> { }
+
+export const DetailScreen = ({ route }: Props) => {
 
   const movie = route.params;
+  const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
 
   return (
-    <View>
-      <Text>Details screen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.imageContainer}>
+        <View style={styles.imageBorder}>
+          <Image
+            source={{ uri }}
+            style={styles.posterImage}
+          />
+        </View>
+      </View>
+
+      <View style={styles.marginContainer}>
+        <Text style={styles.subTitle}>{movie.original_title}</Text>
+        <Text style={styles.title}>{movie.title}</Text>
+      </View>
+
+      <View style={styles.marginContainer}>
+        <Icon name="star-outline"
+          color="grey"
+          size={20}
+        />
+      </View>
+    </ScrollView>
   )
 }
 
-export default DetailScreen
+const styles = StyleSheet.create({
+  imageContainer: {
+    width: '100%',
+    height: screenHeight * 0.7,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.24,
+    shadowRadius: 7,
+    elevation: 10,
+    borderBottomStartRadius: 25,
+    borderBottomEndRadius: 25
+  },
+  imageBorder: {
+    flex: 1,
+    overflow: 'hidden',
+    borderBottomStartRadius: 25,
+    borderBottomEndRadius: 25
+  },
+  posterImage: {
+    flex: 1
+  },
+  marginContainer: {
+    marginHorizontal: 20,
+    marginTop: 20
+  },
+  subTitle: {
+    fontSize: 16,
+    opacity: 0.8
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold'
+  }
+});
